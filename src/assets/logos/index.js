77074ecf54d30ea2e53a8a111f2928287aad9b42
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-27 17:28:57 +0530

    fix about section

--- src/assets/logos/index.js
+++ src/assets/logos/index.js
@@ -24,5 +24,6 @@ import threejs from './threejs.svg'
 import visualstudiocode from './visualstudiocode.svg'
 import vitejs from './vitejs.svg'
 import wordpress from './wordpress.svg'
+import typescript from './typescript.svg'
 
-export { auth0, azure, blazor,blazorPink,cplusplus,csharpPink, csharp,css3,dotnet,dotnetPink,dotnetcore,efcore,react,git,github,html5,javscript,microsoft,microsoftsqlserver,sqlite,stripe,tailwindcss,threejs,visualstudiocode,vitejs,wordpress};
+export { auth0, azure, blazor,blazorPink,cplusplus,csharpPink, csharp,css3,dotnet,dotnetPink,dotnetcore,efcore,react,git,github,html5,javscript,microsoft,microsoftsqlserver,sqlite,stripe,tailwindcss,threejs,visualstudiocode,vitejs,wordpress,typescript};
