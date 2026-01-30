3eee11280a6b251692f1bc99f4c0e2c361c32964
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2026-01-30 13:39:23 +0530

    fix bug

--- src/assets/socials/index.js
+++ src/assets/socials/index.js
@@ -1,6 +1,6 @@
 import instagram from './instagram.svg';
 import whatsApp from './whatsApp.svg';
-import linkedin from './linkedin.svg';
+import linkedin from './linkedIn.svg';
 import gmail from './gmail.svg'
 
 export { instagram, whatsApp, linkedin, gmail };
